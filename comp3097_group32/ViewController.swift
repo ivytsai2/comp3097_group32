cc698b2917844d04360df27bd696b1afebd46380
Ivy Tsai <ivytsai2@gmail.com>
2023-02-22 18:20:57 -0500

    initial project and lunchscreen

---
+++ comp3097_group32/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  comp3097_group32
+//
+//  Created by Ivy Tsai on 2023-02-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
